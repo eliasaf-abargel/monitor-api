429b43914a844440de79cea2fc0de4d76b730679
Eliasaf Abargel <eliasafabargel@icloud.com>
2024-01-22 10:58:28 +0200

    send log for server logstash

--- server-api.py
+++ server-api.py
@@ -1,52 +1,53 @@
 import json
+import os
 from datetime import datetime
 import jsonschema
 from flask import Flask, request, jsonify
 from jsonschema import validate
+import requests  # for sending data to Logstash
 
 app = Flask(__name__)
 
-API_KEY = 'JY8LVI4fQNS-raAnzs5Y4Q'
+API_KEY = os.environ.get("API_KEY", "JY8LVI4fQNS-raAnzs5Y4Q")  # Fallback to a default
 
-# Load the JSON schema for the error reports from an external file
-with open('error_schema.json', 'r') as schema_file:
-    error_report_schema = json.load(schema_file)
+# Load the schema outside of the route to avoid repeated file reads.
+try:
+    with open('error_schema.json', 'r') as schema_file:
+        error_report_schema = json.load(schema_file)
+except Exception as e:
+    print(f"Error loading schema: {e}")
+    exit(1)
 
-
-def is_valid_date(timestamp):
-    try:
-        datetime.fromisoformat(timestamp)
-        return True
-    except ValueError:
-        return False
-
-
-@app.route('/report_error', methods=['POST'])
+@app.route('/index-log', methods=['POST'])
 def report_error():
-    api_key = request.headers.get('API-Key')
+    api_key = request.headers.get("API-Key")
 
     if api_key != API_KEY:
         return jsonify({"error": "Unauthorized"}), 401
 
-    error_data = request.json
+    log_data = request.json
 
     try:
-        validate(instance=error_data, schema=error_report_schema)
-
-        if not is_valid_date(error_data["timestamp"]):
-            raise ValueError("Invalid timestamp format.")
-
+        validate(instance=log_data, schema=error_report_schema)
     except jsonschema.exceptions.ValidationError as e:
         return jsonify({"error": "Bad Request", "message": str(e)}), 400
-    except ValueError as e:
-        return jsonify({"error": "Bad Request", "message": str(e)}), 400
 
-    with open('error_log.json', 'a') as file:
-        json.dump(error_data, file)
-        file.write('\n')
+    log_data["timestamp"] = datetime.utcnow().isoformat()
 
-    return jsonify({"status": "success"}), 200
+    # Save log data to file
+    try:
+        with open('error_log.json', 'a') as file:
+            file.write(json.dumps(log_data) + '\n')
+    except IOError as e:
+        return jsonify({"error": "Failed to write to log file", "message": str(e)}), 500
 
+    # Send log data to Logstash
+    try:
+        requests.post("http://192.168.203.34:5055", json=log_data)
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": "Logstash Connection Failed", "message": str(e)}), 500
+
+    return jsonify(dict(status="success")), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001, host='0.0.0.0')
