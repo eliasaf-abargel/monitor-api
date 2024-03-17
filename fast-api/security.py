578fa9eefcc34ee8b0cacf7959cea4d66df964a1
eliasaf-abargel <eliasafabargel@icloud.com>
2024-03-17 13:06:48 +0200

    add url website logs

--- fast-api/security.py
+++ fast-api/security.py
@@ -4,9 +4,13 @@ API_KEYS = {
     os.getenv('CAROLINA_API_KEY', 'default_carolina_key'): "/index-log/carolinaui",
     os.getenv('COMAX_API_KEY', 'default_comax_key'): "/index-log/comax",
     os.getenv('WEB_API_KEY', 'default_web_key'): "/index-log/web",
-    os.getenv('WS_API_KEY', 'default_web_key'): "/index-log/ws",
-    os.getenv('WS-API_API_KEY', 'default_web_key'): "/index-log/ws-api",
-    os.getenv('RAMI-API_API_KEY', 'default_web_key'): "/index-log/RAMI-api",
+    os.getenv('WS_API_KEY', 'default_ws_key'): "/index-log/ws",
+    os.getenv('WS-API_KEY', 'default_ws-api_key'): "/index-log/ws-api",
+    os.getenv('RAMI-API_KEY', 'default_rami-api_key'): "/index-log/RAMI-api",
+    os.getenv('UMax-API_KEY', 'default_UMax-api_key'): "/index-log/UMax-api",
+    os.getenv('BSB-API_KEY', 'default_BSB-api_key'): "/index-log/BSB-api",
+    os.getenv('BinaPos-API_KEY', 'default_BinaPos-api_key'): "/index-log/BinaPos-api",
+
 }
 
 DEFAULT_KEY = os.getenv("API_KEY", "default-key")
